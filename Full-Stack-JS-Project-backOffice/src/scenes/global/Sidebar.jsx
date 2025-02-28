1fdbecf62102227a39b71dd1d31eae4754901631
med oussema boussida <medoussemaboussida@gmail.com>
2025-02-28 15:35:47 +0100

    Update Sidebar.jsx

--- Full-Stack-JS-Project-backOffice/src/scenes/global/Sidebar.jsx
+++ Full-Stack-JS-Project-backOffice/src/scenes/global/Sidebar.jsx
@@ -115,7 +115,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >         
-                    {user ? user.username : "Chargement..."} {/* Affichage du nom d'utilisateur ou message de chargement */}
+                    {user ? user.username : "Chargement..."}
                   </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
