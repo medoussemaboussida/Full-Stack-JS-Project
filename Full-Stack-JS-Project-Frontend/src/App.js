de2a479004f2a8a484ac77b0013a4a3a146acb2a
Yasssa02 <yassinedhahbi65@esprit.tn>
2025-03-04 15:42:38 +0100

    Integrate Publication Page

--- Full-Stack-JS-Project-Frontend/src/App.js
+++ Full-Stack-JS-Project-Frontend/src/App.js
@@ -7,7 +7,9 @@ import Register from './pages/Register';
 import Forgot_Password from './pages/Forgot-Password';
 import DetailsStudent from './pages/details-student';
 import Reset_Password from './pages/Reset-Password';
-import AccountDisabled from './pages/AccountDisabled'
+import AccountDisabled from './pages/AccountDisabled';
+import Publication from './pages/Publication';
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 
@@ -28,6 +30,7 @@ function App(){
         <Route path="/reset-password/:token" element={<Reset_Password />} />
 
         <Route path="/AccountDisabled" element={<AccountDisabled />} />
+        <Route path="/Publication" element={<Publication />} />
 
         </Routes>
       </BrowserRouter>
