172eceae11f0fe4e31b9a8d5a3b4150c647f5c6e
Yasssa02 <yassinedhahbi65@esprit.tn>
2025-04-10 01:45:47 +0100

    Fix mail

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -18,13 +18,13 @@ const sendEmail = async (to, subject, htmlContent) => {
         const signature = `
             <hr>
             <p style="font-size:14px;color:#555;">Cordialement,<br>
-            <strong>UnmindCare Team</strong><br>
-            <a href="http://unmindcare.com" style="color:#007bff;">www.unmindcare.com</a></p>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Logo_ESPRIT_Ariana.jpg" alt="UnmindCare Logo" width="150">
+            <strong>EspritCare</strong><br>
+            <a href="http://EspritCare.com" style="color:#007bff;">www.EspritCare.com</a></p>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/ff/Logo_ESPRIT_Ariana.jpg" alt="EspritCare Logo" width="150">
         `;
 
         let mailOptions = {
-            from: `"UnmindCare" <${process.env.EMAIL_USER}>`, // ✅ Nom de l'expéditeur
+            from: `"EspritCare" <${process.env.EMAIL_USER}>`, // ✅ Nom de l'expéditeur
             to: to,
             subject: subject,
             html: htmlContent + signature // ✅ Ajoute la signature à l’email
