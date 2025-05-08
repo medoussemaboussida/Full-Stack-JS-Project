28f10ee3ab838f5d2c99ec00110b81ff1741867c
Yasssa02 <yassinedhahbi65@esprit.tn>
2025-05-08 12:16:34 +0100

    fixx

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: ['<rootDir>/tests/**/*.test.js'],
-  testPathIgnorePatterns: ['<rootDir>/Full-Stack-JS-Project/'],
+  testPathIgnorePatterns: ['<rootDir>/Full-Stack-JS-Project-Frontend/'],
   setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
   testTimeout: 30000, // Global timeout for all tests
   verbose: true,
